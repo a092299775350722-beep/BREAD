d7a9d4d882deadfadcba56533944c50f06c70d7f
吳儀岑 <n111048034@mail.nchu.edu.tw>
2026-05-13 17:34:48 +0800

    fix: 移除 base 路徑以修復 Vercel 上的 CSS 樣式2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
-  // 1. 換成你的 GitHub 帳號
-  site: 'https://你的GitHub帳號.github.io', 
-  // 2. 換成你在 GitHub 上建立的 Repo 名稱 (注意前面要有一個斜線)
-  base: '/你的儲存庫名稱', 
+  // ⚠️ 因為改用 Vercel 部署，所以把下面這兩行刪除！
+  // site: 'https://你的GitHub帳號.github.io', 
+  // base: '/你的儲存庫名稱', 
   
   vite: {
     plugins: [tailwindcss()]
